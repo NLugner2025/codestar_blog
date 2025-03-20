8df4876407ab501250d36b1470fecce4eacb38c7
Nlugner-GitPod <nils@lugner.uk>
2025-03-20 16:15:14 +0000

    added templates

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-
 def my_blog(request):
     return HttpResponse("Hello, Blog!", content_type="text/plain")
+template_name = "blog/index.html"
+paginate_by = 6
