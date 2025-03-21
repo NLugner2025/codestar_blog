6a70d6a7ae59ff7ff75312a9e01a6af075269a4f
Nlugner-GitPod <nils@lugner.uk>
2025-03-21 09:42:06 +0000

    updated database for deployment

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,20 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:20]
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
+    
 )
     content = models.TextField()
 created_on = models.DateTimeField(auto_now_add=True)
